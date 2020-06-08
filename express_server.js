cf98e89b2c75c492083238b38b920a4998078aa9
Maribelle <maribelleleongzz@gmail.com>
2020-06-08 20:42:23 -0300

    created edit form

--- express_server.js
+++ express_server.js
@@ -32,7 +32,6 @@ app.get('/urls/:shortURL', (req, res) => {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
-  console.log(templateVars.longURL);
   if (templateVars.longURL) {
     res.render('urls_show', templateVars);
   } else {
