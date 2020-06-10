9a80884846a186bc7a07a2d245770baa65a79322
Maribelle <maribelleleongzz@gmail.com>
2020-06-10 15:55:36 -0300

    converted data of shortURL in urlDB to an object

--- storeData/seedData.js
+++ storeData/seedData.js
@@ -1,6 +1,11 @@
+// const urlDatabase = {
+//     b2xVn2: 'http://www.youtube.com',
+//     '9sm5xK': 'http://www.google.com',
+// };
+
 const urlDatabase = {
-    b2xVn2: 'http://www.youtube.com',
-    '9sm5xK': 'http://www.google.com',
+    b2xVn2: { longURL: 'http://www.youtube.com', userID: 'jdoe' },
+    '9sm5xK': { longURL: 'http://www.google.com', userID: 'mdoe' },
 };
 
 const users = {
