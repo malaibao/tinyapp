9260950cfb0e5587e1e9c6017a2c6045d48ba680
Maribelle <maribelleleongzz@gmail.com>
2020-06-11 14:11:55 -0300

    added mocha chai testing getUserByEmail function

---
+++ test/helperTest.js
@@ -0,0 +1,18 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../controllers/controllers');
+
+describe('getUserByEmail', function () {
+    it('should return a user with valid email', function () {
+        // jdoe exists in users DB
+        const user = getUserByEmail("jdoe@gmail.com");
+        const expectedOutput = "jdoe";
+        assert.equal(user.id, expectedOutput);
+    });
+
+    it('should return undefined with non-existent email', function () {
+        // abcd does not exist in users DB
+        const user = getUserByEmail("abcd@gmail.com");
+        assert.equal(user, undefined);
+    });
+});
